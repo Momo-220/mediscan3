95a15e64f2051612a531718cf3a61eb526faf7d1
MOMO <seinimohamed01@email.com>
2025-03-06 16:03:02 +0300

    mise 2.1

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ document.addEventListener('DOMContentLoaded', () => {
 18. Fabricant
 19. Recommandations pour la durée de validité`;
 
-            const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`, {
+            const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
